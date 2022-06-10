675bae3a9df41651e42b0b83eb7de20fdbebc791
crzorz <crzorz@outlook.com>
2022-06-10 14:11:29 +0800

    新增swift工程的Framework模板

---
+++ {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/Source/{{cookiecutter.product_name}}.swift
@@ -0,0 +1,8 @@
+//
+//  {{cookiecutter.product_name}}.swift
+//  {{cookiecutter.product_name}}
+//
+//  Created by crzorz on 2021/12/02.
+//  Copyright © 2021 {{cookiecutter.organization_name}}. All rights reserved.
+//
+
---
+++ {{cookiecutter.product_name}}/{{cookiecutter.product_name}}Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{cookiecutter.product_name}}Demo
+//
+//  Created by crzorz on 2021/12/02.
+//  Copyright © 2021 {{cookiecutter.organization_name}}. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
