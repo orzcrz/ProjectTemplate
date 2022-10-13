f70023233300f5464f6d49dc8b5a19da90bcbc47
crzorz <crzorz@outlook.com>
2022-10-13 17:54:24 +0800

    去掉点冗余成分

--- {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/Sources/{{cookiecutter.product_name}}.h
+++ {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/Sources/{{cookiecutter.product_name}}.h
@@ -7,11 +7,3 @@
 //
 
 #import <Foundation/Foundation.h>
-
-//! Project version number for {{cookiecutter.product_name}}.
-FOUNDATION_EXPORT double {{cookiecutter.product_name}}VersionNumber;
-
-//! Project version string for {{cookiecutter.product_name}}.
-FOUNDATION_EXPORT const unsigned char {{cookiecutter.product_name}}VersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <{{cookiecutter.product_name}}/PublicHeader.h>
