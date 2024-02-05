b46f8bed580cb30dc9c5e6c66096db5291795bc4
crzorz <crzorz@outlook.com>
2024-02-05 13:10:50 +0800

    update date formatter

--- {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/Sources/{{cookiecutter.product_name}}.h
+++ {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/Sources/{{cookiecutter.product_name}}.h
@@ -2,8 +2,8 @@
 //  {{cookiecutter.product_name}}.h
 //  {{cookiecutter.product_name}}
 //
-//  Created by crzorz on 2021/12/02.
-//  Copyright © 2021 {{cookiecutter.organization_name}}. All rights reserved.
+//  Created by crzorz on {% now 'local', '%Y/%m/%d' %}.
+//  Copyright © {% now 'local', '%Y' %} {{cookiecutter.organization_name}}. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
--- {{cookiecutter.product_name}}/{{cookiecutter.product_name}}Demo/SceneDelegate.m
+++ {{cookiecutter.product_name}}/{{cookiecutter.product_name}}Demo/SceneDelegate.m
@@ -2,8 +2,8 @@
 //  SceneDelegate.m
 //  {{cookiecutter.product_name}}Demo
 //
-//  Created by crzorz on 2021/12/02.
-//  Copyright © 2021 {{cookiecutter.organization_name}}. All rights reserved.
+//  Created by crzorz on {% now 'local', '%Y/%m/%d' %}.
+//  Copyright © {% now 'local', '%Y' %} {{cookiecutter.organization_name}}. All rights reserved.
 //
 
 #import "SceneDelegate.h"
