b74c8c887fa1c04990c8a1ba0b574e878c115e9a
crzorz <crzorz@outlook.com>
2024-02-05 13:08:02 +0800

    update date formatter

--- {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/Sources/{{cookiecutter.product_name}}.swift
+++ {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/Sources/{{cookiecutter.product_name}}.swift
@@ -2,7 +2,7 @@
 //  {{cookiecutter.product_name}}.swift
 //  {{cookiecutter.product_name}}
 //
-//  Created by crzorz on 2021/12/02.
-//  Copyright © 2021 {{cookiecutter.organization_name}}. All rights reserved.
+//  Created by crzorz on {% now 'local', '%Y/%m/%d' %}.
+//  Copyright © {% now 'local', '%Y' %} {{cookiecutter.organization_name}}. All rights reserved.
 //
 
