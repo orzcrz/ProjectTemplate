f5f2c550ccddc4b17e6b099cdac2aeba0bf5eb0e
crzorz <crzorz@outlook.com>
2024-07-29 14:26:45 +0800

    😼增加Test模板

---
+++ {{cookiecutter.product_name}}/{{cookiecutter.product_name}}/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  {{cookiecutter.product_name}}
+//
+//  Created by {{cookiecutter.author_name}} on {% now 'local', '%Y/%m/%d' %}.
+//  Copyright © {% now 'local', '%Y' %} {{cookiecutter.organization_name}}. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+    }
+
+
+}
+
